0f85f2975173cfcf3d9cd3095557462a44a8695e
phamduy0165 <75572336+phamduy0165@users.noreply.github.com>
2021-06-03 13:16:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 10h anh call nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: https://www.facebook.com/messages/t/100029607845271/
+   
 }
