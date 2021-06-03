c633b5dfc18e6d1a3ba016dba7b91135d73f9ab5
thanhphac0308 <85274245+thanhphac0308@users.noreply.github.com>
2021-06-03 12:44:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Fat <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Không, Fat à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho Fat <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu Thủy nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Fat biết mà 🥰. Yêu Thủy nhiều nhiều 😘😘',
+    messDesc: 'Yêu công chúa.',
+    btnAccept: 'Yêu Fat <3 ',
+    messLink: 'http://fb.com/thanhphac0308' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
