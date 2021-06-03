ae6f4a835ffc6196e38e640f94fe2b7e47642677
giang12366 <85265977+giang12366@users.noreply.github.com>
2021-06-03 09:23:34 +0700

    Update config.js
    CONFIG = {
        introTitle: 'Babe cúc à!',
        introDesc: `Trái đất vốn lạ thường
        Mà sao a cứ đi nhầm đường
        Lạc vào tim iêm lẻ loi
        Đằng sau chữ yêu đây là thương`,
        btnIntro: 'hihi',
        title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
        desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
        btnYes: 'Vẫn cứ là thích cúc <33',
        btnNo: 'Không, Anh trai à :3',
        question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
        btnReply: 'Gửi cho anh cúc ánh haha <3',
        reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
        mess: 'em biết mà 🥰. Yêu anhh nhiều nhiều 😘😘',
        messDesc: 'Tối nay 7h iêm qua vp  đón nhé ,
        btnAccept: 'Okiiiii lun <3',
        messLink: https://www.facebook.com/pii.giang.775
    }

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Babe cúc à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao a cứ đi nhầm đường
+    Lạc vào tim iêm lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vẫn cứ là thích cúc <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho anh cúc ánh haha <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'em biết mà 🥰. Yêu anhh nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h iêm qua vp  đón nhé ,
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: https://www.facebook.com/pii.giang.775
 }
