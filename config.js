d98262910a716c065f8b00bd513f3ef69f59b7bd
ThuyTrangNeee <85302726+ThuyTrangNeee@users.noreply.github.com>
2021-06-04 11:05:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,11 +9,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Chị yêu em <33',
     btnNo: 'Chị không yêu em:3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em ???',
     btnReply: 'Lời nhắn gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Yêu chị nhiều nhiều 😘😘',
     messDesc: 'Tối nay chị đi chơi với em nhé.',
-    btnAccept: 'Câu trả lời của chị <3',
-    messLink: 'http://www.facebook.com/trangho2301' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnAccept: 'Oke nèeee <3',
+    messLink: 'http://m.me/trangho2301' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
