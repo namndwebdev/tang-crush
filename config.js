521074d36be279c3867592a82a9731ad5e5dc36f
Minh Anh <85324974+MinhAnh-Hub@users.noreply.github.com>
2021-06-04 10:44:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,4 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com'
-
+}
