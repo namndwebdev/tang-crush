155e751fd04405eaa98b76b87787595f293e174f
QuangDu1 <57499507+QuangDu1@users.noreply.github.com>
2021-06-04 19:58:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Broo à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'Minh Thu à!',
+    introDesc: `Văn ít thôi em `,
+    btnIntro: 'Nhấn vào đây',
+    title: 'Thu nờ on 😙',
+    desc: '??? Nờ on non ',
+    btnYes: 'Thích anh Dự <33',
+    btnNo: 'Không là con chó :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Chuyển cho anh 100tr luôn nè , yêu anh ',
+    mess: 'Anh biết mà 🥰. Tốt đấy 😘😘',
+    messDesc: 'STK anh là : xxxxxxxxxxxxxxxxxx.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/babykamion' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
