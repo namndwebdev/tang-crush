fb27c53e5e3b5caa6ffdf6257583f5378bb9e381
nguyenlam107196 <85325985+nguyenlam107196@users.noreply.github.com>
2021-06-14 23:29:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Phương Trang à!',
+    introTitle: 'Phương Trang à!!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
