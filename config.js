24803d8973ebf583a8d760960e316cf9d5a75769
huydq161801 <55663815+huydq161801@users.noreply.github.com>
2021-06-26 16:05:38 +0700

    oki
    oki

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là yêu anh <33',
-    btnNo: 'Không, Đâu Anh trai à :3',
+    btnNo: 'Không, Đâu anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
