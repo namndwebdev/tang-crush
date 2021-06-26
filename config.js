928f97a1d4261c164829ae067d3f97206de99f16
huydq161801 <55663815+huydq161801@users.noreply.github.com>
2021-06-26 15:54:51 +0700

    cmt
    cmt

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Thi tốt nghiệp , anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100018185173268' 
 }
