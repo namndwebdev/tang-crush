f0008c315d9d1002ad72034bf36c49f5146eb6ed
Dukvodich <86740361+Dukvodich@users.noreply.github.com>
2021-07-01 01:31:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Bae à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Quỳnh à!',
+    introDesc: `Cậu à cho tớ xin lỗi đi tớ biết lỗi rồi mà :((`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Phải chăng tớ đã muốn làm bạn cậu từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng tớ đã thấy cậu tốt bụng ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là tha cho Duk <33',
+    btnNo: 'Không, thằng loz à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu quý tớ <3',
+    btnReply: 'Gửi cho Duk <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Em đúng là công chúa của anh',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Cậu đúng là bạn iu của tớ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
