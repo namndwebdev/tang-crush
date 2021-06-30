6c2c81d46e8e8679971164e9681d601a4038c7a8
thjgarghlrw <86715254+thjgarghlrw@users.noreply.github.com>
2021-06-30 16:57:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'bé iu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'gửi cho bạn NGUYỄN THỊ DIỄM YẾN 😙',
+    desc: 'tao muốn quay lại dí mày  ',
+    btnYes: 'quay lại nha <33',
+    btnNo: 'Không quay lại à:3',
+    question:'Trên thế giới hơn 7 tỉ người mà em lại ko chịu làm lại chớ <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'anh sẽ ko đối xử tốt vs em sẽ ko giống hồi đó .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
