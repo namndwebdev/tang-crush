84ae35d6de8ff1c6523758b72fa841bfeeaff113
trangnhung0810 <86727670+trangnhung0810@users.noreply.github.com>
2021-06-30 22:11:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Hellu em iu!',
-    introDesc: `Chị có điều muốn hỏi
-    em nhớ phải trả lời thật lòng nhaaa:3`,
-    btnIntro: 'ok',
-    title: 'Em yêu chị lắm phải không nàoooo 😙',
-    desc: 'Nếu em không trả lời mà thoát ra thì tức là muốn làm chồng chị đó nhaaa ',
-    btnYes: ' Vâng ạ! yêu chị nhiều vllllll <33',
-    btnNo: 'Không, chị hâm à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
-    btnReply: 'Gửi cho chị iu <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Chị biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Còn h thì chần chờ gì nữaaa, nhắn lại cho chị lý do sao iu chị điii.',
+    introTitle: 'Hellu bạn iu!',
+    introDesc: `t có điều muốn hỏi
+    m nhớ phải trả lời thật lòng nhaaa:3`,
+    btnIntro: 'nhất trí',
+    title: 'M quý t lắm phải không nàoooo 😙',
+    desc: 'Nếu m không trả lời mà thoát ra thì tức là muốn m sẽ phải làm nô tỳ cho t ',
+    btnYes: ' Chuẩn duma quý vllll <33',
+    btnNo: 'Không, m điên à :3',
+    question:'Nói đi sao lại quý t thế <3',
+    btnReply: 'Gửi Nhung xinh <3',
+    reply: 'Tại m xinh gái, hiền lành, dễ thương, cute',
+    mess: 'T biết mà 🥰. m quý t vl 😘😘',
+    messDesc: 'Còn h thì ib nói lý do thật điii.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/hi.sun.56' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
