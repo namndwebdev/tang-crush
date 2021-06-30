6ec22cff49e0da9c48d1be0dc0d988218e8a1895
Vanky3009 <86727110+Vanky3009@users.noreply.github.com>
2021-07-01 02:59:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Thu Hiền à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: `Trái đất vốn lạ  thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
