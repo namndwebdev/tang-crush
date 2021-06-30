fe282c9b31b8e32d59de73ab9fb5291064deab25
Fjji1 <86725498+Fjji1@users.noreply.github.com>
2021-06-30 20:17:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Ae à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    Mà sao ae mình cứ đi nhầm đường
+    Lạc vào tim của nhau
+    Đằng sau chữ bạn đây là thương`,
+    btnIntro: 'hihi 😎',
+    title: 'Phải chăng ae mình đã chơi với nhau từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng ae mình đã chơi ngay với nhau từ lúc thấy tình bạn đẹp ấy ',
+    btnYes: 'Vẫn cứ là chơi với tui <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh em mình chơi với nhau <3',
+    btnReply: 'Gửi cho ae <3',
+    reply: 'Chơi thì chơi mà không chơi thì chơi <33333333',
+    mess: 'Tuu biết mà 🥰.Ae vẫn chơi với tuu 😘😘',
+    messDesc: 'Từ nay ae mình vẫn chơi chung👍👍.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    
