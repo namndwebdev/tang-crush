c11ed7d4afd8c4e421f85ab758f0d53fe9116594
Quynhnhu03 <86771878+Quynhnhu03@users.noreply.github.com>
2021-07-01 16:37:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Anh à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
