fc5bdcfc2cf7cee57c4b910d1e1904cfd406ae4d
ntthienn <86788266+ntthienn@users.noreply.github.com>
2021-07-01 21:22:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Heluuuu !',
+    introDesc: `Chắc đang cười mỉm rồi đây nàyy`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    title: 'Thôiii khong cười nữaa',
+    desc: 'Bún xuynhh đúng khum 🥺 ',
+    btnYes: 'Vẫn cứ là thích chuẩn <33',
+    btnNo: 'Không, Bún xấuu lắm :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Lạc lại biết Bún nhỏoo?',
+    btnReply: 'Trả lời hẳn hoyy 👊🏻',
+    reply: 'Bún quá đáng iuu lun 🥺',
+    mess: 'Người ta biết mà 😌. Cái điều nó quá nà hiển nhiên luôn 🙊',
+    messDesc: 'Đánh nhau khumm 👊🏻',
+    btnAccept: 'Nhàooo zô',
+    
