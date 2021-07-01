69ba90c1031338354cae112cf396372078243e63
phap230901 <86772189+phap230901@users.noreply.github.com>
2021-07-01 16:38:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Thảo à!',
+    introTitle: 'Thảo  à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,7 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Yêu nhiều <33',
-    btnNo: 'Không yêu :3',
-    messLink: 'https://www.facebook.com/Fapdeptrai
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Hôm nào ta gặp nhau nhaa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/Fapdeptrai' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
