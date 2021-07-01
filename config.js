5908513fbb88956074dc14cbad2b5424aec1d247
CaoHieuNguyen <86795261+CaoHieuNguyen@users.noreply.github.com>
2021-07-01 23:41:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Helu Linh!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Linh cứ đi nhầm đường
+    Lạc vào tim Hiếu lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnIntro: 'hehe',
+    title: 'Phải chăng Linh đã yêu Hiếu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Linh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Hiếu <33',
+    btnNo: 'Deo, Bạn mơ à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Linh lại yêu Hiếu <3',
+    btnReply: 'Gửi cho Hiếu <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Hiếu biết mà   Yêu Linh nhiều nhiều 😘😘',
+    messDesc: 'Làm congchua của Hiếu Không ❤️.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
