10d4ee70a42b8e51f9b359be0b9871f505a22f5d
Dyduyml <86763725+Dyduyml@users.noreply.github.com>
2021-07-01 13:01:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Anh yêu à !👹',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ ăn cức hoài vậy💩
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'cục cức🖕',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnYes: 'anh yêu cục cưng của anh nhiều ơi là nhiều luôn á 😘',
+    btnNo: 'Không . anh yêu em ít á cục cưng của anh❤',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh chỉ yêu mình em hoi 🤭',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Bắt buộc phải yêu nha đỷ 🐶',
+    mess: 'Em biết mà 🥰. Anh ko thể nào rời xa em được hihi 😘😘',
+    messDesc: 'Tối nay 9h làm nháy nhá bấy bề 😘.',
+    btnAccept: 'Dạ dạ thưa vợ yêu 🤩<3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
