80ece0e33cc61163728fef8b4cc13ca500e79a61
Thachthao21 <86756891+Thachthao21@users.noreply.github.com>
2021-07-01 10:16:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    introTitle: 'He luu !',
+    introDesc: `Tớ hỏi cậu cái này nhớ phải trả lời thật lòngg nhaaa
+    `,
+    btnIntro: 'aukeyy',
+    title: 'Cậu thích tớ đúng hongg nee🥰',
+    desc: 'Thoát ra là thích tớ dokk🐳 ',
+    btnYes: 'Um thich cau nhiu nhiu <33',
+    btnNo: 'Không, nằm mơ ii :3',
+    question:'Tớ biết cậu chọn thích mà,seo cậu lại thích tớ zii <3',
+    btnReply: 'Gửi cho thachthao🐳',
+    reply: 'Tại vì cậu cutee đo' <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Mãiii iuu.',
+    btnAccept: 'Okiiiii <3',
+    messLink: 'https://www.facebook.com/tran.thachthao.2109'
+    }
