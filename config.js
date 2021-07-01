db5726c3ea8e38ade9c943d3f2a123f3bbe7ec51
Salt1234 <86760479+Salt1234@users.noreply.github.com>
2021-07-01 12:36:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không mơ đi',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì đâu cần lý do đâu🥰',
+    reply: 'Yêu thì đâu cần lý do đâu ',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Thi xong nhớ cho ôm nha nhớ lắm rùi😋',
     btnAccept: 'Okiiiii lun 😉',
