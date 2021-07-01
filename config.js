a03f7cf134754ebf528d68ccf4c5111581070cea
BrightWine <86771675+BrightWine@users.noreply.github.com>
2021-07-01 17:31:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hê lu Tùng ngố đang đọc cái này 🤣',
+    introDesc: `Gọi anh là cục Wifi
+    vì mất anh là mất mạng 🤣😅`,
+    btnIntro: 'Hihihi',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy 😘',
+    btnYes: 'Tất nhiên rùiii 😍',
+    btnNo: 'Mơ đi em 😝',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em 😝',
+    btnReply: 'Gửi cho em 😝',
+    reply: 'Vì Diệu Minh của anh cute xinh gái nhất trên đời ❤️',
+    mess: 'Đừng khen em, em ngại 🥰 Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Giờ thì nhắn tin cho em thôi nàoooo',
+    btnAccept: 'Okeeeeee lun ❤️',
+    messLink: 'https://www.facebook.com/messages/t/100023733193093' 
 }
