da22a5bba206d0b24a8f270091212695503043ee
Vkook31 <86761300+Vkook31@users.noreply.github.com>
2021-07-01 12:28:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hey bro!',
+    introDesc: `Dẫu khoảng cách từ I đến U  xa xôi đến thế 
+     Fuck JKLMNOPQRST
+        Mình vẫn vượt muôn vàn chữ cái để chạm tới cậu`,
+
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'i want u tell me u love me😙',
+    desc: 'i love u 3000',
+    btnYes: 'Thích mình đúng khum <33',
+    btnNo: 'No, bro à :3',
+    question:'Bạn có muốn hẹn hò với mình khum <3',
+    btnReply: 'Gửi cho mình nhá <3',
+    reply: 'Yessssss <33333333',
+    mess: 'Mình biết mà 🥰. Love u 😘😘',
+    messDesc: 'Hãy nhắn tin cho mình nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/hey.baby.3104'//
 }
