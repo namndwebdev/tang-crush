fb1fa9dc1079dd05a1a857e51067492a784f43c5
0976862143 <86790824+0976862143@users.noreply.github.com>
2021-07-02 06:49:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'cđl kia',
+    introDesc: `mày thử chơi ma tuý chộn nước mắm khi nghe nhạc Sơn Tùng chưa`,
+    btnIntro: 'rồi nka',
+    title: 'do you feel the love?',
+    desc: 'spend the summer of a lifetime with me'
+    btnYes: 'i love you babe 😏',
+    btnNo: 'không đâu cđl à',
+    question:'nói nhỏ cho nghe tao là 1 hokage ',
+    btnReply: 'gửi cho người đẹp trai nhất thế giới',
+    reply: 'pónk thì pónk mà không pónk thì pónk',
+    mess: '🐒',
+    messDesc: 'tối nay mua tao gói bim bim nka   ',
+    btnAccept: 'oke ank iu ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
