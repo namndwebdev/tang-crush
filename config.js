90ead6b896f302654d0f509a917cce97039dbec5
MinnnNgg <86762177+MinnnNgg@users.noreply.github.com>
2021-07-01 13:29:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    introTitle: 'Sơnn à :>>',
+    introDesc: `Thật tuyệt vời là em đã sử dụng biết bao thời gian để học hành và chơi game, giá mà em cũng sử dụng 1 phần đó để quan tâm chị`,
+    btnIntro: 'hjcccc',
+    title: 'Emm có thuwnn chị hămmmm',
+    desc: 'Out ra là munsn làm chồng chị  ',
+    btnYes: 'Coasaaa thưn chị lemm',
+    btnNo: 'hông em chỉ xem chị như một ng quan trọng thoiii',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
+    btnReply: 'Gửi cho chị <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Chị biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'tối choii game nheee',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
