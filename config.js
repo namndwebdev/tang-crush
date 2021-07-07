648c2985f20a105f9b6e40e353d5e46ca16ee75b
vuvyetanh <87032556+vuvyetanh@users.noreply.github.com>
2021-07-07 13:57:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Uyên lợn Uyên thối à!',
+    introDesc: `Uyên lợn Uyên thối`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Uyên lợn Uyên thối 😙',
+    desc: 'Uyên lợn Uyên thối ',
+    btnYes: 'Uyên lợn Uyên thối <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    question:'Uyên lợn Uyên thối <3',
+    btnReply: 'Uyên lợn Uyên thối<3',
+    reply: 'YUyên lợn Uyên thối <33333333',
+    mess: 'Uyên lợn Uyên thối 😘😘',
+    messDesc: 'Uyên lợn Uyên thối.',
+    btnAccept: 'Uyên lợn Uyên thối <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
