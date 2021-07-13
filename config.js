3bc5c8335bb766c440faed8ae8971cf718d33cd9
buihoa04 <87375990+buihoa04@users.noreply.github.com>
2021-07-13 20:37:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Huyền à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Huyền cứ đi nhầm đường
+    Lạc vào tim mình lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Phải chăng Huyền đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Huyền đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Hòa <33',
+    btnNo: 'Không, Hòa à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Huyền lại thích mình <3',
+    btnReply: 'Gửi cho Hòa <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Hòa biết mà 🥰. Yêu Huyền nhiều nhiều 😘😘',
+    messDesc: 'Làm bạn gái mình nhé!.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/profile.php?id=100050014350665
 }
