3774852ccace772478c8e334cc9ee51dac1d1d08
baobao16122003 <87383878+baobao16122003@users.noreply.github.com>
2021-07-13 23:39:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `mn vẫn`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
