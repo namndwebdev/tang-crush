f5a63ce2723d20e060d0123933eb1ee8ed949e4d
duckmannzzd <87533788+duckmannzzd@users.noreply.github.com>
2021-07-16 21:07:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Baby so cute của anh à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
