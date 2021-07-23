c458b498dc42a78fd214d4bd0b2008843412b2eb
jasmine040116 <87864021+jasmine040116@users.noreply.github.com>
2021-07-23 20:46:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Tớ biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'nhớ các babae quá chời.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'mi tu <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
