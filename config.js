fb50f437f81d54003953547b1d888eb3e54ccdfc
Tranhung12 <86721468+Tranhung12@users.noreply.github.com>
2021-08-26 13:21:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `chị à :< chị biết em thương chị lắm hong hả `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
