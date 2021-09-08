f946d47dd80a7d9c800e3a493132ff384b08eebc
Phongly59 <89494871+Phongly59@users.noreply.github.com>
2021-09-08 20:31:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Nóc Nhà à',
+    introDesc: `Em phải trả lời câu hỏi này của a :3`,
+    btnIntro: 'dzaaaaa',
+    title: 'Em yêu anh rất nhiêu đúng khum?',
+    desc: 'a bt đáp án rùi :)) ',
+    btnYes: 'em yêu a rất nhìu',
+    btnNo: 'Không',
+    question:'Hí hí a bt là e yêu anh mà',
+    btnReply: 'Gửi cho em nè',
+    reply: 'thực ra ko nhắn đc đâu, chúc mừng sinh nhật cục cưng',
+    mess: 'A yeeuuuuu em raat nhiều',
+    messDesc: 'Tối nay đặt hộ a tai nghe nhó :)))',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'http://www.facebook.com/profie.php?id=100042213502158
+} 
