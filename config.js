5eadca8289e494f2411a4c1ac8ba85ed2ed177f6
minhnohu <91530654+minhnohu@users.noreply.github.com>
2021-09-28 16:10:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 9h đánh daxua nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/minhnohu
+    messLink: 'https://m.me/minhnohu' //link mess của các bạn. VD: https://m.me/minhnohu
 }
