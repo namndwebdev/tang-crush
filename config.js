367f38f556f0f308fa7f234b37ba8cdddea3f6be
hieudang <85280816+HieuDang15032021@users.noreply.github.com>
2021-11-05 20:26:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Hí bae à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Phải hôm nay là ngày sinh nhật của Thí Lê đúng hong ta 😙',
+    desc: 'Chính xác là ngày 6 tháng 11 năm 2003 nà ',
+    btnYes: 'Đúng đúng đúng',
+    btnNo: 'Không, sai cmnr :3',
+    question:'Sinh nhật này Thí có muốn quà gì hăm??',
+    btnReply: 'Không có gì ngoài tình cảm chân thành này cả',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
