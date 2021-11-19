452c605a8ebbb5b0ee2df5022b8818d408307085
phungductien <94694706+phungductien@users.noreply.github.com>
2021-11-19 21:20:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Em à!',
+    introDesc: `Nhìn em đau là anh biết mình sai, vì anh mà em rơi nước mắt',
+    btnIntro: ':(',
+    title: 'lời xin lối muộn màng và vụng về này anh muốn gửi đến em',
+    desc: 'En cho anh xin lỗi chuyện hôm trước nhé  ',
+    btnYes: 'Dạ anh :333',
+    btnNo: 'Không, Anh trai à :)',
+    question:'một lời xin lỗi chân thành ',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Em tha lỗi cho anh đó ',
+    mess: 'Anh biết mà :3. Yêu em nhiều nhiều <3',
+    messDesc: ' Ib cho anh nhé .',
+    btnAccept: 'Okiiiii lun :33',
+    messLink: 'https://www.facebook.com/ducktien12/
 }
