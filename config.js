43aa42aa7cdccd3b9d34ed9af2a0ad36510a1e3c
cancelban27 <95107475+cancelban27@users.noreply.github.com>
2021-11-27 06:51:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'iuuuu à!',
+    introDesc: `Quốc có đôi lời muốn nói mong iuuuu đọc cho`,
+    btnIntro: 'click nó nhoa',
+    title: 'Quốc đã thích iuuuu từ cái nhìn đầu, đến nay 2 ta cũng đã tìm hiểu được 11 tháng rồi ấy chứ',
+    desc: 'iuuuu có còn chút tình cảm với Quốc hog ? nếu còn iuuuu hãy Tiếp Tục nó nhé😙  ',
+    btnYes: 'tiếp tục <3❤',
+    btnNo: 'không tiếp tục :3',
+    question:'Quốc biết iuuuu đã bùn phiền Quốc quá nhiều
+    mong iuuuu tiếp tục nữa nhaaa',
+    btnReply: 'đây là lời đã suy nghĩ <3',
+    reply: '<333',
+    mess: 'Quốc còn thương iuuuu nhiều lắm ',
+    messDesc: 'nếu được tối nay quốc có thể call với iuuuu được hog?.',
+    btnAccept: 'đây là những gì Quốc đã suy nghĩ 2 ngày qua',
+    messLink: 'https://www.facebook.com/Ba0Qu0cthw/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
