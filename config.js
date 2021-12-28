06dc11c2f44496b066eb2bedea50c18d54e60dfc
mancongchua <96785459+mancongchua@users.noreply.github.com>
2021-12-28 23:06:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'chào cục cưng',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    btnIntro: 'moaaa',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'iu bé lúmm',
+    btnNo: ' hok pé ơi',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu anh em',
+    btnReply: ' hun cái nè',
+    reply: 'iu em lúmmm',
+    mess: ' em pít mà. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'hết dịch mình gặp nhau nhaaa',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://https://www.facebook.com/trantrieu.man308/' 
 }
