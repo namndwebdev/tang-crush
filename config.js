eb3f2e3c91dfadfd332b3e5452987388f1427b64
hoqqdungg28 <106818860+hoqqdungg28@users.noreply.github.com>
2022-06-03 20:13:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hello con người hay dỗi !',
+    introDesc: `Không biết kiếp trước tui có nợ anh gì không
+    mà kiếp này anh ám tui dữ quá =))))`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Hôm nay em có điều muốn nói ',
+    desc: 'không biết ai đó có muốn nghe không<< HiHi>>',
+    btnYes: 'Hôm nay anh sẽ lắng nghe em <<',
+    btnNo: 'không bao giờ =(((',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Em biết mà 🥰. Yêu Anh nhiều nhiều 😘😘',
+    messDesc: 'xin lôi anh nhiều nhé vì những lúc em trẻ con hay giận hờn vu vơ nhưng ngược lại anh thay vì trách em thì anh luôn dịu dàng vơi em  ',
+    btnAccept: 'okki lun <<',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
