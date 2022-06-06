dd526aff670a30e3a44dfcc5e7b1f5e5ab663e8e
Sosi0601 <94071681+Sosi0601@users.noreply.github.com>
2022-06-07 00:49:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'em iu à!',
+    introDesc: `anh iu em nhiều nhất trên đời,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'iu anh <3',
+    btnNo: 'hong iu <3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/100008695961757'
+    mess: 'Anh biết mà 🥰. Yêu em rất nhiều 😘😘',
+    messDesc: 'only btram :3',
+    btnAccept: 'iuuuuuuuuuuuu',
+    messLink: 'https://www.facebook.com/profile.php?id=100008695961757'
 }
