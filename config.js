eaaa976f5efca9c05a4c241a309064cb57cb4de0
Dat nguyn <89145165+Batado@users.noreply.github.com>
2022-06-21 17:50:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Halo, hòn tử đít pự của em ',
+    introDesc: `tui biết bé học hành mệt mỏi suy nghĩ đủ thứ nên tui làm cái này cho bé  `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Tui có rất nhiều kế hoạch để làm cùng với bé lắm á ',
+    desc: 'Tui yêu bé rất nhiều ',
+    btnYes: 'Vẫn cứ yêu bé vãi l cho dù địt thúi đi nữa thì vẫn iu <33',
+    btnNo: 'Đố bé  nhấn đc chữ ko ó nka  :3',
+    question:'Soa bé lại yêu tui é  <3',
+    btnReply: 'Gửi cho bé  ó  <3',
+    reply: 'Yêu thì yêu mà không yêu cũng yêu  <33333333',
+    mess: ' biết mà 🥰. Hhehehe  😘😘',
+    messDesc: ' Yêu bé vãi l    :))).',
+    btnAccept: 'ôki lun <3',
+    messLink: 'http://m.me/dat.nguyn.92754'
 }
