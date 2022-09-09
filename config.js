52c87fdeb12cc91ee3c22165cd933ea80e2534de
quaniube <113191428+quaniube@users.noreply.github.com>
2022-09-09 21:43:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hellu bé',
+    introDesc: `Tui iu bé nhiều lắm á bé bít hok.`,
+    btnIntro: 'Dạ',
+    title: 'Bé đồng ý iu tui nha ',
+    desc: 'Hok trả lời là cắn á ',
+    btnYes: 'Iu ơi là Iu <3',
+    btnNo: 'Mơ à',
+    question:'Nói cho tui lí do bé thích tui iiii :vvvv',
+    btnReply: 'Gửi cho quân <3',
+    reply: 'Vì quân đẹp trai cute đáng iu vl iu quân',
+    mess: 'Tui iu bé siu siu nhiều lắm ó',
+    messDesc: 'Mai qua đón bé ii chơi nha.',
+    btnAccept: 'Okay IU <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100028608391913' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
