49609b5b18741987d30598c81cb6c117d3e6bd3c
Yang Dae Hyun <94821068+Sasuketaminn@users.noreply.github.com>
2022-09-30 13:28:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Yến à!',
-    introDesc: `Bỏ lỡ hoàng hôn thì có
-    thể ngắm lại vào ngày mai. Còn
-    bỏ lỡ Yếnn thì không có cơ
-    hội lần hai.`,
+    introTitle: 'Ng0cLinh à!',
+    introDesc: `Bớt cute được không :v.`,
     btnIntro: 'hihi',
     title: 'Ăn cơm má nấu thôi có cần cute vậy hông 😙',
-    desc: 'Không liên quan nhưng mà Yến có muốn hang out with me hông ',
+    desc: 'Ngoclinh có muốn hang out with me không 👈',
     btnYes: 'Có <3333',
-    btnNo: 'Hông :3',
-    question:'Trên thế giới hơn 7 tỉ người gặp được Yến là một phép màu',
+    btnNo: 'Khum nha :3',
+    question:'Trên thế giới hơn 7 tỉ người gặp được Ng0cLinh là một phép màu',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Okay hết dịch tớ đi chơi cùng cậu <33333333',
+    reply: 'Tớ cũng thấy như vậy <33333333',
     mess: 'Tớ biết mà 🥰',
-    messDesc: 'Hết dịch hẹn Yến đi coi phim, Hưng qua đón nha.',
+    messDesc: 'Khi nào rảnh Ng0cLinh nhớ hẹn đi cafe cùng tớ nha 🥰.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/yangdaehyunnn' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.instagram.com/ydh.kr82_/'
 }
