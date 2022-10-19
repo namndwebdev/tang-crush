37b73008947d76eafc6e56bdb88a5729e59a9ccc
MinhPC9 <116027851+MinhPC9@users.noreply.github.com>
2022-10-19 23:36:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Hello bae. Anh Minh đây!',
-    introDesc: `Em có tin vào tình yêu sét đánh không ?
-    Đó là lí do chúng ta gặp nhau
-    Và anh đã yêu em mất rồi
+    introTitle: 'Hê sờ nhô babe!',
+    introDesc: `Thùy lợn. Em có biết nay ngày gì không ?
     Hãy tiếp tục để có thể thấy anh ngọt ngào đến mức nào <3`,
     btnIntro: 'Dạ, hihi đồ ngốc',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Khiến em nhung nhớ anh cả đêm lẫn ngày ? Thoát ra là đồng ý đó',
-    btnYes: 'Đúng rồi đó babe <33',
+    title: '20-10 nè anh chúc cô gái của anh mọi điều tốt đẹp nhất 😙',
+    desc: 'Nhân ngày 20-10 này, anh muốn chúc cô gái luôn hạnh phúc bên anh. Em yêu có yêu anh không ?',
+    btnYes: 'Rất nà yêu anh luôn <33',
     btnNo: 'Không, Anh mơ à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'20-10 này em muốn được nhận quà gì nè <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Bởi vì anh là cả thế giới của em đó đồ ngốc <3333333333',
+    reply: 'Chỉ cần bên anh là đủ rồi. Em đâu cần quà <3333333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tới đây và nói những lời yêu thương với anh nào babe <3',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Anh đùa thôi, mai gặp nhé cô bé anh vẫn có quà cho cô bé nè <3',
+    btnAccept: 'Ô sờ kê anh yêu. Cho anh cái hẹn ở đây nhé <3',
     messLink: 'http://m.me/congminhh410'
 }
