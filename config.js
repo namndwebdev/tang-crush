9c848872983845359a845aba8f9657b1af3f8e2f
QuangDu1 <57499507+QuangDu1@users.noreply.github.com>
2022-11-19 23:40:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,67 +1,16 @@
-
-<!DOCTYPE html>
-<html lang="vi">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Love</title>
-    <link rel="stylesheet" href="main.css">
-    <link rel="shortcut icon" href="./icon.png" type="image/x-icon">
-</head>
-<body>
-    <div class="grid"></div>
-
-    <div class="warning"></div>
-
-    <div class="base">
-        <button id="activate">
-            <span></span>
-        </button>
-    </div>
-
-    <div class="box opened" id="cover">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <span></span><span></span>
-    </div>
-
-    <div class="hinges"></div>
-
-    <div class="text">
-        SELF-&thinsp;DESTRUCT
-    </div>
-    
-    <div id="panel2">
-        <div id="msg">YOUR ANSWER IS FALSE</div>
-        <span id="again">AGAIN PLEASE</span>
-    </div>
-
-
-    <div id="panel">
-        <div id="msg">ANSWER NOW</div>
-        <div id="time">9</div>
-        <span id="abort"><a href="index2.html">YES</a></span>
-        <span id="no">NO</span>
-        <span id="detonate">Oc Cho</span>
-    </div>
-
-    <div id="turn-off"></div>
-    <div id="closing"></div>
-
-    <div id="restart"><button id="reload"></button></div>
-
-    <div id="mute"></div>
-
-    <audio id="alarm">
-        <source src="https://josetxu.com/demos/sounds/self-destruct-alarm-count.mp3" type="audio/mpeg">
-    </audio>
-    
-    <script src="main.js"></script>
-
-</body>
-</html>
+const CONFIG = {
+    introTitle: 'Ngân à!',
+    introDesc: `Rất vui được quen biết Ngân`,
+    btnIntro: 'hihi',
+    title: 'Ghé Qua 😙',
+    desc: 'Già Cùng nhau là được  ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Vì cậu là người chân thành và tử tế.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/MamaDontLikeMe8888' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
