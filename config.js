4e995ea2fa2a846e5233ddec89b52de5b0ee07d9
ThangCodeTaoLao <120721854+ThangCodeTaoLao@users.noreply.github.com>
2022-12-16 19:04:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,3 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
