eecb63299e8b8b4fdaa5ab59893fd63925cc383f
QuangDu1 <57499507+QuangDu1@users.noreply.github.com>
2023-03-28 23:32:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    introTitle: 'Ngân à!',
-    introDesc: `Rất vui được quen biết Ngân`,
+    introTitle: 'Phùng Thị Thu Hằng à!',
+    introDesc: `Rất vui được quen biết Hằng`,
     btnIntro: 'hihi',
     title: 'Ghé Qua 😙',
     desc: 'Già Cùng nhau là được  ',
@@ -9,8 +9,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Vì cậu là người chân thành và tử tế.',
+    mess: 'Anh biết mà 🥰. Thích em nhiều nhiều 😘😘',
+    messDesc: 'Vì anh là người chân thành và tử tế.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/MamaDontLikeMe8888' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
