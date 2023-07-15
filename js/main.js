f54f3d0ad118d40f6cb55230bae5f19d39c3a593
GnouhP <64305504+GnouhP-TV@users.noreply.github.com>
2023-07-16 00:10:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
-$(document).ready(function() {
-    // process bar
-    setTimeout(function() {
-        firstQuestion();
-        $('.spinner').fadeOut();
-        $('#preloader').delay(350).fadeOut('slow');
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        });
-    }, 600);
-})
+
 
 function init(){
     $('#title').text(CONFIG.title)
